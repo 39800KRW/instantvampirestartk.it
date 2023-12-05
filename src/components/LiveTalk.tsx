1a823129f8a7525def7aeea41ca8190784ab83ea
Alex4386 <psh010209@gmail.com>
2023-12-05 13:51:24 +0900

    feat: add marquee to livetalk

--- src/components/LiveTalk.tsx
+++ src/components/LiveTalk.tsx
@@ -5,12 +5,12 @@ const LiveTalk: FC<LiveChatDataInterface> = ({ phone, message }) => {
   return (
     <div className="p-2 px-4 w-full bg-zinc-50 text-zinc-600">
       <div className="flex flex-row gap-2 text-[.7rem] overflow-hidden text-ellipsis">
-        <span className="font-semibold">라이브톡</span>
+        <span className="font-semibold shrink-0 break-keep whitespace-pre">라이브톡</span>
         <div className="flex flex-row gap-1">
           <span className="text-zinc-400 font-semibold">{phone}</span>
-          <span className="w-full overflow-hidden text-ellipsis break-keep whitespace-pre">
-            {message}
-          </span>
+          <div className="w-full overflow-hidden text-ellipsis break-keep whitespace-pre">
+            <span className={message.length > 20 ? "shortmarquee-content" : ""}>{message}</span>
+          </div>
         </div>
       </div>
     </div>
