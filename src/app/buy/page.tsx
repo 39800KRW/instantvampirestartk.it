05c5b3a19e00b9d2c3b2d4eca84671b2ddde1ab9
Alex4386 <psh010209@gmail.com>
2024-09-10 23:39:51 +0900

    chore: redirect warning via client-side javascript

--- src/app/buy/page.tsx
+++ src/app/buy/page.tsx
@@ -1,11 +1,19 @@
-import { redirect } from 'next';
+'use client';
+
+import { useEffect } from 'react';
 
 export default function Home() {
-  // 예전에 EZ2AC Reboot
-  // redirect('https://store.steampowered.com/app/2325460/EZ2ON_REBOOT__R__ENDLESS_CIRCULATION/', 302);
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      // window.location.href = 'https://store.steampowered.com/app/2325460/EZ2ON_REBOOT__R__ENDLESS_CIRCULATION/';
 
-  // 김장미는 통신판매업 미등록으로, 판매를 할 수 없습니다.
-  // 애초에 식약처 허가를 받지 않은 불법식품 및 허위과대광고잖아?
-  // ===[ BAN! ]===
-  redirect('http://warning.or.kr/i6.html', 302);
+      // 김장미는 통신판매업 미등록으로, 판매를 할 수 없습니다.
+      // 애초에 식약처 허가를 받지 않은 불법식품 및 허위과대광고잖아?
+      // ===[ BAN! ]===
+
+      window.location.href = 'http://warning.or.kr/i6.html';
+    }
+  }, []);
+
+  return <></>;
 }
