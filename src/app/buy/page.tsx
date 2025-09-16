7d56a97cc7fce0c0ac8efcd93d97a672446c126f
Alex4386 <psh010209@gmail.com>
2025-09-16 19:01:12 +0900

    fix: eslint

--- src/app/buy/page.tsx
+++ src/app/buy/page.tsx
@@ -6,7 +6,8 @@ export default function Home() {
   useEffect(() => {
     if (typeof window !== 'undefined') {
       // 다시 EC 로 보내버리고
-      window.location.href = 'https://store.steampowered.com/app/2325460/EZ2ON_REBOOT__R__ENDLESS_CIRCULATION/';
+      window.location.href =
+        'https://store.steampowered.com/app/2325460/EZ2ON_REBOOT__R__ENDLESS_CIRCULATION/';
 
       // 김장미는 통신판매업 미등록으로, 판매를 할 수 없습니다.
       // 애초에 식약처 허가를 받지 않은 불법식품 및 허위과대광고잖아?
@@ -24,4 +25,3 @@ export default function Home() {
 
   return <></>;
 }
-
